c645552f0fbf7634b5824d1dc371e1d086c17829
Kimtaegi <kim76002192@gmail.com>
2024-02-06 15:23:34 +0900

    test

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from st_pages import Page, show_pages
 st.set_page_config(
   layout="wide",  # 더 많은 공간을 위한 넓은 레이아웃 설정
 )
-# # 아이콘을 포함한 페이지 링크 생성
+
+# 아이콘을 포함한 페이지 링크 생성
 # st.page_link("pages/1_👋_Hi There.py", label="Hi There", icon="👋")
 # st.page_link("pages/2_1️⃣_노후 건물 분포도.py", label="노후 건물 분포도", icon="1️⃣")
 # st.page_link("pages/3_2️⃣_노후 건물 거래 동향.py", label="노후 건물 거래 동향", icon="2️⃣")
 # st.page_link("pages/4_3️⃣_노후 건물 평당 가격.py", label="노후 건물 평당 가격", icon="3️⃣")
+# st.page_link("pages/5_4️⃣_관련 정보 가져오기.py", label="관련 기사 가져오기", icon="4️⃣")
 # st.page_link("https://github.com/Kimtae00/real-estate-dashboard-team5", label="Github", icon="🚀")
 
 # Optional -- adds the title and icon to the current page
@@ -18,12 +20,11 @@ st.set_page_config(
 show_pages(
     [
         Page("app.py", "real estate project team5", "🏢"),
-        Page("pages/1_👋_Hi There.py", "Hi There"),
-        Page("pages/2_1️⃣_노후 건물 분포도.py", "노후 건물 분포도", "1️⃣"),
-        Page("pages/3_2️⃣_노후 건물 거래 동향.py", "노후 건물 거래 동향", "2️⃣"),
-        Page("pages/4_3️⃣_노후 건물 평당 가격.py", "노후 건물 평당 가격", "3️⃣"),
-        Page("pages/5_4️⃣_관련 정보 가져오기.py", "관련 기사 가져오기", "4️⃣"),
-        
+        # Page("pages/1_👋_Hi There.py", "Hi There"),
+        Page("pages/2_app.py", "노후 건물 분포도", "1️⃣"),
+        Page("pages/3_app.py", "노후 건물 거래 동향", "2️⃣"),
+        Page("pages/4_app.py", "노후 건물 평당 가격", "3️⃣"),
+        # Page("pages/5_4️⃣_관련 정보 가져오기.py", "관련 기사 가져오기", "4️⃣"),        
     ]
 )
 
